41447ea37a1fe05926de34a040ccf82f356f8244
luciacanalda <luciacanaldas@gmail.com>
2024-10-23 18:06:11 +0000

    First Change

---
+++ domain.js
@@ -0,0 +1,17 @@
+let multiplicationTable = [];
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+for (let i=0; i < pronoun.length;i++) {
+
+    for (let j=0; j < adj.length; j++){
+        for (let h=0; h < noun.length;h++) {
+            console.log(`${pronoun[i]}${adj[j]}${noun[h]}.com`);
+    
+      
+    
+    }
+}
+    }
